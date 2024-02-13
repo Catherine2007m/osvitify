81d9cb004dd700c1bf2470bc6d77964ac968cec5
Catherine2007m <132053050+Catherine2007m@users.noreply.github.com>
2024-02-13 13:52:21 +0200

    Update random.js

--- random.js
+++ random.js
@@ -1,9 +1,9 @@
 // List of paragraphs
         var paragraphs = [
-            "Today is a great day!",
-            "The weather is perfect today.",
-            "I'm learning something new every day.",
-            "Excited to see what today brings."
+            "Пийте 1,5-2 літри води на день; можна випити хоча б півсклянки одразу після пробудження.",
+            "Відчуття вдячності підвищує рівень настрою; це можна відчути, якщо почати дякувати за кілька добрих подій або речей навколо щодня.",
+            "Короткі ролики в Інтернеті знижують рівень нашої концентрації. Ми в прямому сенсі не зможемо спокійно читати або дивитися фільми через довгий час сидіння в Рілсах або ТікТоці. ",
+            "Існує гігієна розуму. Так, і це значить - увага до тієї інформації, що ми отримуємо впродовж дня. Чи можете ви згадати, що дивилися, читали, писали за сьогодні? Наскільки це знадобилося?"
         ];
 
         // Function to get a random paragraph
@@ -13,7 +13,7 @@
 
         // Function to update the content
         function updateContent() {
-            var header = "<h3>ĐĐ¸ĐżĐ°Đ´ĐşĐžĐ˛Đ¸Đš ŃĐ°ĐşŃ</h3>";
+            var header = "<h3>Випадковий факт</h3>";
             var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 var today = new Date().toLocaleDateString('uk-UA', options);
             var paragraph = "<p>" + getRandomParagraph() + "</p>";
