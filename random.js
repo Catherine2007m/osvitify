af4eb6e05055146d90b9a2505983564cc1cb5dba
Catherine2007m <132053050+Catherine2007m@users.noreply.github.com>
2024-02-16 10:30:46 +0200

    Actualizar random.js

--- random.js
+++ random.js
@@ -17,7 +17,7 @@
             var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 var today = new Date().toLocaleDateString('uk-UA', options);
 var yourtime = "<p1>Час в Україні:</p1>";
-var time = newDate().toLocaleString('uk-UA', { timeZone: "Europe/Kiev" });           
+var time = new Date().toLocaleString('uk-UA', { timeZone: "Europe/Kiev" });           
 var paragraph = "<p1>" + getRandomParagraph() + "</p1>";
             document.getElementById("content").innerHTML = "<p1>" + today + "</p1>" + "<p1>" + yourtime + time + "</p1>" + header + paragraph;
         }
